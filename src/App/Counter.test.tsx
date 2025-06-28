0696d3e1d959875f8ff97fa67724c97e6b8d6c63
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 11:44:58 +0200

    add case

--- src/App/Counter.test.tsx
+++ src/App/Counter.test.tsx
@@ -1,3 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Counter } from './Counter';
+import { expect, it } from 'vitest';
+
+it('shows initial state', () => {
+  render(<Counter start={4} />);
+
+  const count = screen.getByText('Count: 4');
+
+  expect(count).toBeInTheDocument();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   test: {
+    environment: 'happy-dom',
     setupFiles: ['./vitest-setup.ts'],
   },
 });
