9b32b41f2dab8bc77591fbe60d46f64d2d9cf43f
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 13:39:54 +0200

    extract

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,20 +4,20 @@ import { assert } from 'chai';
 import { useEffect } from 'react';
 import { Counter } from './Counter';
 
+async function test() {
+  await new Promise((r) => setTimeout(r, 0)); // React "Should not already be working"
+
+  const screen = render(<Counter start={0} />);
+  const count = screen.getByText('Count: 0');
+
+  const increment = screen.getByRole('button', { name: /Inc/ });
+  await userEvent.click(increment);
+
+  assert.equal(count.innerText, 'Count: 1');
+}
+
 export function App() {
   useEffect(() => {
-    async function test() {
-      await new Promise((r) => setTimeout(r, 0)); // React "Should not already be working"
-
-      const screen = render(<Counter start={0} />);
-      const count = screen.getByText('Count: 0');
-
-      const increment = screen.getByRole('button', { name: /Inc/ });
-      await userEvent.click(increment);
-
-      assert.equal(count.innerText, 'Count: 1');
-    }
-
     test();
   }, []);
 
