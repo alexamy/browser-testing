1acde7d6c1d96001a4fb2e1b0c947073b0885d6a
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 11:52:53 +0200

    refactor

--- src/App/Counter.tsx
+++ src/App/Counter.tsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 export function Counter({ start }: { start: number }) {
   const [count, setCount] = useState(start);
 
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(Math.max(0, count - 1));
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button disabled={count === 0} onClick={() => setCount(Math.max(0, count - 1))}>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement} disabled={count === 0}>
         -
       </button>
     </div>
