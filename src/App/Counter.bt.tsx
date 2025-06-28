69a699b018480ace1d97eaaf11bb48baeb15501a
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 18:38:54 +0200

    move other tests

--- src/App/Counter.bt.tsx
+++ src/App/Counter.bt.tsx
@@ -2,6 +2,14 @@ import { expect } from 'chai';
 import { Counter } from './Counter';
 import { render, it, userEvent } from '../Framework';
 
+it('shows initial state', async () => {
+  const screen = render(<Counter start={4} />);
+
+  const count = screen.queryByText('Count: 4');
+
+  expect(count).not.equal(null);
+});
+
 it('increments', async () => {
   const screen = render(<Counter start={0} />);
   const count = screen.getByText('Count: 0');
@@ -18,8 +26,25 @@ it('decrements', async () => {
   const screen = render(<Counter start={5} />);
   const count = screen.getByText('Count: 5');
 
-  const increment = screen.getByText('Dec');
-  await userEvent.click(increment);
+  const decrement = screen.getByRole('button', { name: /Dec/ });
+  await userEvent.click(decrement);
 
   expect(count.innerText).equals('Count: 4');
+});
+
+it('can be only positive', async () => {
+  const screen = render(<Counter start={0} />);
+
+  const decrement = screen.getByRole('button', { name: /Dec/ });
+  await userEvent.click(decrement);
+
+  const count = screen.queryByText('Count: 0');
+  expect(count).not.equal(null);
+});
+
+it('decrement is disabled at 0', async () => {
+  const screen = render(<Counter start={0} />);
+
+  const decrement = screen.getByRole('button', { name: /Dec/ }) as HTMLButtonElement;
+  expect(decrement.disabled).equals(true);
 });
