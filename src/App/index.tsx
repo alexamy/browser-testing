94a3f87403b1ccc5f0a8439a989e8d4d6458233d
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 13:28:47 +0200

    add chai assert

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,23 @@
 import { Counter } from './Counter';
 
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useEffect } from 'react';
+import { assert } from 'chai';
+
 export function App() {
+  useEffect(() => {
+    async function test() {
+      const count = screen.getByText('Count: 0');
+      const increment = screen.getByText('Inc');
+      await userEvent.click(increment);
+
+      assert.equal(count.innerText, 'Count: 2');
+    }
+
+    test();
+  }, []);
+
   return (
     <div>
       <Counter start={0} />
