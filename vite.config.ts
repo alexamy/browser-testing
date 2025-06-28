d91b4506fa71ae3b6f69f64709c94978991bdb2c
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-28 13:34:05 +0200

    add full reload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, type Plugin } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+
+function reloadPageOnHMR(): Plugin {
+  return {
+    name: 'full-reload',
+    handleHotUpdate({ server }) {
+      server.ws.send({ type: 'full-reload' });
+      return [];
+    },
+  };
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reloadPageOnHMR()],
   test: {
     environment: 'happy-dom',
     setupFiles: ['./vitest-setup.ts'],
