0e72b63a0a5b554ad0a650a29b3bbfb1da74bfbd
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-29 13:16:54 +0200

    add convert case

--- src/Framework/vite-bt-plugin/generatorTransform.js
+++ src/Framework/vite-bt-plugin/generatorTransform.js
@@ -1,10 +1,12 @@
 // Transform second argument of `it` from async function to async generator,
 // yielding line number for each statement.
-export const generatorTransformPlugin = {
-  name: 'test body duplicator',
-  visitor: {
-    CallExpression: generatorTransform,
-  },
-};
+export default function generatorTransformPlugin() {
+  return {
+    name: 'test body duplicator',
+    visitor: {
+      CallExpression: generatorTransform,
+    },
+  };
+}
 
 function generatorTransform(path) {}
--- src/Framework/vite-bt-plugin/transform.test.ts
+++ src/Framework/vite-bt-plugin/transform.test.ts
@@ -5,6 +5,8 @@ import { expect, it } from 'vitest';
 
 // @ts-expect-error No types for plugin
 import bodyDuplicatorPlugin from './bodyDuplicator.js';
+// @ts-expect-error No types for plugin
+import generatorTransformPlugin from './generatorTransform.js';
 
 async function readTsx(filePath: string) {
   const resolvedFilePath = path.join(import.meta.dirname, filePath);
@@ -22,6 +24,26 @@ it('duplicates method code', async () => {
     filename: 'fixture.tsx',
     presets: ['@babel/preset-typescript'],
     plugins: [bodyDuplicatorPlugin],
+    generatorOpts: {
+      retainLines: true,
+    },
+  });
+
+  if (!transformed || !transformed.code) {
+    throw new Error('Failed to transform code by Babel.');
+  }
+
+  expect(transformed.code).toEqual(output);
+});
+
+it('convert async function to async generator', async () => {
+  const input = await readTsx('./fixtures/generatorTransform/input.fixture.tsx');
+  const output = await readTsx('./fixtures/generatorTransform/output.fixture.tsx');
+
+  const transformed = await babel.transformAsync(input, {
+    filename: 'fixture.tsx',
+    presets: ['@babel/preset-typescript'],
+    plugins: [generatorTransformPlugin],
     generatorOpts: {
       retainLines: true,
     },
