30c1a1615aa586bc5305d8ca859feac4460afb15
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-29 12:33:37 +0200

    add body duplicator

---
+++ src/Framework/vite-bt-plugin/fixtures/bodyDuplicator/output.fixture.tsx
@@ -0,0 +1,30 @@
+// @ts-nocheck
+
+import { it } from '@framework';
+
+const x = 1;
+const y = 2;
+
+it('increments', async () => {
+  const screen = render(<Counter start={0} />);
+  const count = screen.getByText('Count: 0');
+
+  const increment = screen.getByRole('button', { name: /Inc/ });
+
+  for (let i = 0; i < 3; i++) {
+    await userEvent.click(increment);
+  }
+
+  expect(count.innerText).equals('Count: 3');
+}, [
+`const screen = render(<Counter start={0} />);`,
+`const count = screen.getByText('Count: 0');`,
+``,
+`const increment = screen.getByRole('button', { name: /Inc/ });`,
+``,
+`for (let i = 0; i < 3; i++) {`,
+`  await userEvent.click(increment);`,
+`}`,
+``,
+`expect(count.innerText).equals('Count: 3');  `,
+]);
