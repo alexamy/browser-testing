23d8aa57c0b701ad0d3fef9bc297a35e5ca7d256
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-29 12:34:10 +0200

    add generator

---
+++ src/Framework/vite-bt-plugin/fixtures/generatorTransform/output.fixture.tsx
@@ -0,0 +1,25 @@
+// @ts-nocheck
+
+import { it } from '@framework';
+
+const x = 1;
+const y = 2;
+
+it('increments', async function* test() {
+  yield 0;
+  const screen = render(<Counter start={0} />);
+  yield 1;
+  const count = screen.getByText('Count: 0');
+
+  yield 3;
+  const increment = screen.getByRole('button', { name: /Inc/ });
+
+  yield 5;
+  for (let i = 0; i < 3; i++) {
+    yield 6;
+    await userEvent.click(increment);
+  }
+
+  yield 9;
+  expect(count.innerText).equals('Count: 3');
+});
