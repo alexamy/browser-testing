a51d7b1e997e467671e45c907a07bd0865207e75
Aleksandr Chashchin <ac@happynumbers.com>
2025-06-29 12:28:20 +0200

    add body duplicator

--- src/Framework/vite-bt-plugin/bodyDuplicator.js
+++ src/Framework/vite-bt-plugin/bodyDuplicator.js
@@ -1,38 +1,38 @@
-// Duplicate the `'it` second argument function body
-// to array of strings, provided as third argument to `it` call.
+// Duplicate the `it` second argument function body
+// to array of strings, provided as a third argument to `it` call.
 export const bodyDuplicatorPlugin = {
-  name: 'body duplicator',
-  visitor: bodyDuplicator,
-};
-
-const bodyDuplicator = {
-  CallExpression(path) {
-    // Check if this is an 'it' call with exactly 2 arguments
-    const isItCall = t.isIdentifier(path.node.callee, { name: 'it' });
-    const hasTwoArguments = path.node.arguments.length === 2;
-    const isTestFunction = isItCall && hasTwoArguments;
-    if (!isTestFunction) return;
-
-    // Check if the second argument is an async arrow function or function expression
-    const secondArg = path.node.arguments[1];
-    const isFunction = t.isArrowFunctionExpression(secondArg) || t.isFunctionExpression(secondArg);
-    const isProperMethod = isFunction && secondArg.async;
-    if (!isProperMethod) return;
-
-    // Get line numbers of block
-    const funcBody = secondArg.body;
-    const start = funcBody.loc.start.line;
-    const end = funcBody.loc.end.line;
-
-    // Get code for line numbers
-    const sourceCode = path.hub.file.code;
-    const sourceLines = sourceCode.split('\n');
-    const codeLines = sourceLines.slice(start, end - 1);
-    const codeLinesIndent = codeLines.map((line) => line.replace(/^\s{2}/, ''));
-
-    // Add the line contents array as the third argument
-    const stringLiterals = codeLinesIndent.map((line) => t.stringLiteral(line));
-    const lineContentsArray = t.arrayExpression(stringLiterals);
-    path.node.arguments.push(lineContentsArray);
+  name: 'test body duplicator',
+  visitor: {
+    CallExpression: bodyDuplicator,
   },
 };
+
+function bodyDuplicator(path) {
+  // Check if this is an 'it' call with exactly 2 arguments
+  const isItCall = t.isIdentifier(path.node.callee, { name: 'it' });
+  const hasTwoArguments = path.node.arguments.length === 2;
+  const isTestFunction = isItCall && hasTwoArguments;
+  if (!isTestFunction) return;
+
+  // Check if the second argument is an async arrow function or function expression
+  const secondArg = path.node.arguments[1];
+  const isFunction = t.isArrowFunctionExpression(secondArg) || t.isFunctionExpression(secondArg);
+  const isProperMethod = isFunction && secondArg.async;
+  if (!isProperMethod) return;
+
+  // Get line numbers of block
+  const funcBody = secondArg.body;
+  const start = funcBody.loc.start.line;
+  const end = funcBody.loc.end.line;
+
+  // Get code for line numbers
+  const sourceCode = path.hub.file.code;
+  const sourceLines = sourceCode.split('\n');
+  const codeLines = sourceLines.slice(start, end - 1);
+  const codeLinesIndent = codeLines.map((line) => line.replace(/^\s{2}/, ''));
+
+  // Add the line contents array as the third argument
+  const stringLiterals = codeLinesIndent.map((line) => t.stringLiteral(line));
+  const lineContentsArray = t.arrayExpression(stringLiterals);
+  path.node.arguments.push(lineContentsArray);
+}
