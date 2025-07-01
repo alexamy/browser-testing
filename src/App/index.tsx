88d114910944ee6c7e8a599901c6049cb78ae788
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-01 18:44:22 +0200

    add body

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react';
 import { Route } from './Route';
 
 export function App() {
+  useEffect(() => {
+    const path = window.location.pathname;
+
+    if (path === '/') {
+      document.body.classList.add('ui-root');
+    }
+  }, []);
+
   return (
     <>
       <Route path="/" component={() => <div>Hello!</div>} />
