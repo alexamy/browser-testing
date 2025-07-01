a23c2476d3490ac8a91aa69ad2fe3af5fdfd0a06
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-01 18:38:47 +0200

    rename

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { TestsUI } from './App';
+import { TestsUI } from './App/TestsUI';
 import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
