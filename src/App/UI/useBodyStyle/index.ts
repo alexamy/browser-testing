119913567acc3ea1ebd1cbe156ee01cbfb1fd342
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-01 19:22:13 +0200

    doc

--- src/App/UI/useBodyStyle/index.ts
+++ src/App/UI/useBodyStyle/index.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import s from './body.module.css';
 
+/** Add class to a body element. */
 export function useBodyStyle(className: string) {
   useEffect(() => {
     document.body.classList.add(s[className]);
