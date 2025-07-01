65422bb8cad20c2081b26439c71a24e97a5c3843
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-01 18:39:28 +0200

    add app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-// import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import { TestsUI } from './App/TestsUI';
 import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestsUI />
+    <App />
   </StrictMode>
 );
