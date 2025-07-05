4463569ea6015e7d9b094e055496e51b75a3c42d
Aleksandr Chashchin <achasch@gmail.com>
2025-07-06 00:06:08 +0200

    update fixture

--- src/Framework/vite-bt-plugin/fixtures/pluginOrder/output.fixture.tsx
+++ src/Framework/vite-bt-plugin/fixtures/pluginOrder/output.fixture.tsx
@@ -39,16 +39,13 @@ it(
       yield 1;
       const count = screen.getByText('Count: 0');
       yield 3;
-
       const increment = screen.getByRole('button', { name: /Inc/ });
       yield 5;
-
       for (let i = 0; i < 3; i++) {
         yield 6;
         await userEvent.click(increment);
       }
       yield 9;
-
       expect(count.innerText).equals('Count: 3');
     },
   }
