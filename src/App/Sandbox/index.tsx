c6f1e3e3bac42b7875116a5445696e3fb03b25d3
Aleksandr Chashchin <achasch@gmail.com>
2025-07-06 23:00:22 +0200

    add in progress

--- src/App/Sandbox/index.tsx
+++ src/App/Sandbox/index.tsx
@@ -1,5 +1,6 @@
 import { type TestInstance } from '@framework/test';
 import { useEffect, useState } from 'react';
+import type { IpcEvent } from '../ipc';
 
 function useParent() {
   const [parent, setParent] = useState<Window>();
@@ -20,6 +21,13 @@ export function Sandbox() {
   const [current, setCurrent] = useState<TestInstance>();
   const [inProgress, setInProgress] = useState(false);
   const parent = useParent();
+
+  useEffect(() => {
+    parent?.postMessage({
+      type: 'change-progress',
+      inProgress,
+    } satisfies IpcEvent);
+  }, [parent, inProgress]);
 
   return <></>;
 }
---
+++ src/App/ipc.ts
@@ -0,0 +1,1 @@
+export type IpcEvent = { type: 'change-progress'; inProgress: boolean };
