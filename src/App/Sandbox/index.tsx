20365bb0c1dbcc3bd585b0ef0e58d7087f4060a3
Aleksandr Chashchin <achasch@gmail.com>
2025-07-06 22:53:57 +0200

    add set parent

--- src/App/Sandbox/index.tsx
+++ src/App/Sandbox/index.tsx
@@ -1,4 +1,19 @@
+import { type TestInstance } from '@framework/test';
+import { useEffect, useState } from 'react';
+
 // Empty container to run tests
 export function Sandbox() {
-  return <>Sandbox</>;
+  const [current, setCurrent] = useState<TestInstance>();
+  const [inProgress, setInProgress] = useState(false);
+  const [parent, setParent] = useState<Window>();
+
+  useEffect(() => {
+    if (window.parent === window) {
+      console.warn('Sandbox is meant to be shown in the iframe.');
+    }
+
+    setParent(window.parent);
+  }, []);
+
+  return <></>;
 }
