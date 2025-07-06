5c01673a3babd84c9649bf9eb175de6f2150cecb
Aleksandr Chashchin <achasch@gmail.com>
2025-07-06 22:57:39 +0200

    refactor

--- src/App/Sandbox/index.tsx
+++ src/App/Sandbox/index.tsx
@@ -1,19 +1,25 @@
 import { type TestInstance } from '@framework/test';
 import { useEffect, useState } from 'react';
 
-// Empty container to run tests
-export function Sandbox() {
-  const [current, setCurrent] = useState<TestInstance>();
-  const [inProgress, setInProgress] = useState(false);
+function useParent() {
   const [parent, setParent] = useState<Window>();
 
   useEffect(() => {
     if (window.parent === window) {
-      console.warn('Sandbox is meant to be shown in the iframe.');
+      console.warn('Sandbox is meant to be loaded in the iframe.');
     }
 
     setParent(window.parent);
   }, []);
+
+  return parent;
+}
+
+// Empty container to run tests
+export function Sandbox() {
+  const [current, setCurrent] = useState<TestInstance>();
+  const [inProgress, setInProgress] = useState(false);
+  const parent = useParent();
 
   return <></>;
 }
