c0f8c4c5d2c739a349510fe4d33a3558f4a38a82
Aleksandr Chashchin <ac@happynumbers.com>
2025-07-06 18:35:44 +0200

    extract remove indent

--- src/Framework/vite-bt-plugin/babel/blockDuplicator.ts
+++ src/Framework/vite-bt-plugin/babel/blockDuplicator.ts
@@ -4,16 +4,10 @@ import { types as t } from '@babel/core';
 export function blockDuplicator(codeBlock: t.BlockStatement, sourceCode: string) {
   // Get source code
   const codeLines = extractSourceCode(codeBlock, sourceCode);
+  const codeLinesDeindent = removeExtraIndentation(codeLines);
 
-  // Remove extra indentation
-  const spaceRegex = / {0,}/;
-  const nonEmptyLines = codeLines.filter((line) => line);
-  const indentationLevels = nonEmptyLines.map((line) => line.match(spaceRegex)![0].length);
-  const startIndentation = Math.min(...indentationLevels);
-
-  // Make the line contents array
-  const codeLinesIndent = codeLines.map((line) => line.substring(startIndentation));
-  const lineContentLiterals = codeLinesIndent.map((line) => t.stringLiteral(line));
+  // Make expression
+  const lineContentLiterals = codeLinesDeindent.map((line) => t.stringLiteral(line));
   const lineContentsArray = t.arrayExpression(lineContentLiterals);
 
   return lineContentsArray;
@@ -34,4 +28,14 @@ export function extractSourceCode(statement: t.Statement, sourceCode: string) {
   const codeLines = sourceLines.slice(start, end - 1);
 
   return codeLines;
+}
+
+export function removeExtraIndentation(codeLines: string[]) {
+  const spaceRegex = / {0,}/;
+  const nonEmptyLines = codeLines.filter((line) => line);
+  const indentationLevels = nonEmptyLines.map((line) => line.match(spaceRegex)![0].length);
+  const startIndentation = Math.min(...indentationLevels);
+  const codeLinesDeindent = codeLines.map((line) => line.substring(startIndentation));
+
+  return codeLinesDeindent;
 }
