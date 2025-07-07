4fe3790da99d3eb314397dfd717b1ef278b6f773
Aleksandr Chashchin <achasch@gmail.com>
2025-07-07 22:59:56 +0200

    add frame

--- src/App/UI/SimpleUI.tsx
+++ src/App/UI/SimpleUI.tsx
@@ -1,11 +1,16 @@
 import { useTestsRegistry } from '@framework/react';
 import { useRef } from 'react';
 import { useBodyStyle } from './useBodyStyle';
+import s from './ui.module.css';
 
 export function SimpleUI() {
   const sandbox = useRef<HTMLIFrameElement>(null);
   const tests = useTestsRegistry();
   useBodyStyle('ui');
 
-  return <></>;
+  return (
+    <>
+      <iframe ref={sandbox} src="/sandbox" className={s.frame} />
+    </>
+  );
 }
